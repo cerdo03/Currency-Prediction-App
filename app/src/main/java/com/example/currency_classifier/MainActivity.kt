840720c9922f22ab243be351f77eeb0a68c25963
suryashukla <CerDo3>
2022-01-26 13:22:41 +0530

    OnePlus commit

--- app/src/main/java/com/example/currency_classifier/MainActivity.kt
+++ app/src/main/java/com/example/currency_classifier/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.currency_classifier
 
+import android.Manifest
 import android.app.Activity
 import android.app.Activity.*
 import android.content.Intent
@@ -30,19 +31,27 @@ import java.nio.ByteOrder
 import androidx.appcompat.app.AppCompatActivity.RESULT_OK as RESULT_OK1
 import androidx.appcompat.app.AppCompatActivity.RESULT_OK as RESULT_OK1
 import androidx.appcompat.app.AppCompatActivity.RESULT_OK as RESULT_OK1
+import androidx.core.app.ActivityCompat
+
+import android.content.pm.PackageManager
+
+import androidx.core.content.ContextCompat
+
+
+
 
 private const val FILE_NAME = "photo.jpg"
 private const val REQUEST_CODE = 3
 private lateinit var photoFile: File
 private lateinit var takenImage:Bitmap
 private val dict = mapOf(
-    0 to "2hundred",
+    0 to "2 hundred",
     1 to "hundred",
-    2 to "2thousand",
+    2 to "2 thousand",
     3 to "fifty",
     4 to "twenty",
     5 to "ten",
-    6 to "5hundred"
+    6 to "5 hundred"
 )
 
 class MainActivity : AppCompatActivity() {
@@ -52,22 +61,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(!checkPermission()){
+            requestPermission()
+        }
 
+
+    }
+    private fun checkPermission(): Boolean {
+        return if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted
+            false
+        } else true
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.CAMERA),
+            REQUEST_CODE
+        )
     }
 
     fun getImage(view: android.view.View) {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        photoFile = getPhotoFile(FILE_NAME)
+
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getPhotoFile(FILE_NAME)
 //        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
-        val fileProvider=FileProvider.getUriForFile(this,"com.example.currency_classifier.fileprovider",
-            photoFile)
-        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
-        if(takePictureIntent.resolveActivity(this.packageManager)!=null) {
-            startActivityForResult(takePictureIntent, REQUEST_CODE)
-        }
-        else{
-            Toast.makeText(this,"Unable to open camera",Toast.LENGTH_SHORT).show()
-        }
+            val fileProvider=FileProvider.getUriForFile(this,"com.example.currency_classifier.fileprovider",
+                photoFile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            if(takePictureIntent.resolveActivity(this.packageManager)!=null) {
+                startActivityForResult(takePictureIntent, REQUEST_CODE)
+            }
+            else{
+                Toast.makeText(this,"Unable to open camera",Toast.LENGTH_SHORT).show()
+            }
+
+
 
     }
 
@@ -86,51 +117,52 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun predict(view: View) {
-//        val img = Bitmap.createScaledBitmap(takenImage,224,224,true)
-            val img = takenImage
-        try {
-            val model = Model.newInstance(this)
-
-            // Creates inputs for reference.
-            val inputFeature0 =
-                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
-//            val tensorImage = TensorImage(DataType.FLOAT32)
-//            tensorImage.load(img)
-//            val byteBuffer = tensorImage.buffer
-            val byteBuffer = convertBitmapToByteBuffer(img)
-
-//            val byteBuffer = getByteBufferNormalized(img)
-            Log.d("shape", byteBuffer.toString())
-            Log.d("shape", inputFeature0.buffer.toString())
-
-            if (byteBuffer != null) {
-                inputFeature0.loadBuffer(byteBuffer)
-            }
-
-            // Runs model inference and gets result.
-            val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-
-
-            model.close()
-            val nums = outputFeature0.floatArray
-
-
-            textView.text = dict.get(findInx(nums))
-
-            Log.d("Tag", dict.get(findInx(nums)).toString())
-        } catch (e: IOException) {
-            // TODO Handle the exception
+        if(image_view.drawable==null){
+            Toast.makeText(this,"Click a picture of an Indian Currency Note",Toast.LENGTH_SHORT).show()
         }
+        else{
+            val img = takenImage
+            try {
+                val model = Model.newInstance(this)
+
+
+                val inputFeature0 =
+                    TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+
+                val byteBuffer = convertBitmapToByteBuffer(img)
+
+                Log.d("shape", byteBuffer.toString())
+                Log.d("shape", inputFeature0.buffer.toString())
+
+                if (byteBuffer != null) {
+                    inputFeature0.loadBuffer(byteBuffer)
+                }
+
+
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+
+                model.close()
+                val nums = outputFeature0.floatArray
+                textView.text = dict[findInx(nums)]
+
+
+                Log.d("Tag", dict[findInx(nums)].toString())
+            } catch (e: IOException) {
+                // TODO Handle the exception
+            }
+        }
+
     }
     private fun findInx(arr: FloatArray) :Int{
         var index = 0
-        var min = 0.0f
-
+        var max: Float
+        max = arr[0]
         for(i in 0..arr.size-1){
-            if(arr[i]>min){
-                index = i
-                min = arr[i]
+            if(arr[i]>max){
+                index=i
+                max=arr[i]
             }
         }
         return index
@@ -158,25 +190,6 @@ class MainActivity : AppCompatActivity() {
         return imgData
     }
 
-//    private fun getByteBufferNormalized(bitmapIn: Bitmap): ByteBuffer {
-//        val bitmap = Bitmap.createScaledBitmap(
-//            bitmapIn,
-//            224,
-//            224,
-//            true
-//        )
-//        val width = bitmap.width
-//        val height = bitmap.height
-//        // Below 4 is for floats and 2nd one (1) for grayscale
-//        val mImgData: ByteBuffer = ByteBuffer.allocateDirect(1 * width * height * 1 * 4)
-//        mImgData.order(ByteOrder.nativeOrder())
-//        val pixels = IntArray(width * height)
-//        bitmap.getPixels(pixels, 0, width, 0, 0, width, height)
-//        for (pixel in pixels) {
-//            mImgData.putFloat(Color.blue(pixel).toFloat() / 256.0f)
-//        }
-//        return mImgData
-//    }
 
 
 }
